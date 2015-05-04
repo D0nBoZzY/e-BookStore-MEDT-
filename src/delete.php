32d433ea2a7fdd3ec9647a0ffc3616a43eb39b48
Lukas Zainzinger <lzainzinger@student.tgm.ac.at>
2015-05-04 13:01:06 +0200

    delete Funktion v1
    Erstellen des delete.php

---
+++ src/delete.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  Delete-Funktion um ein File aus dem Server und den dazugehörigen DB-Eintrag zu löschen
+  Autor: lzainzinger
+  Version: 2015-05-04
+*/
+
+include 'import/config.php';
+
+function delete($title){
+  $book = BookQuery::create()->findOneByTitle($title);
+  $file = $book->getPath(); // Pfad zum File am Server
+
+// Löschen des Datenbank - Eintrages
+  $book->delete(); // Löschen in DB
+
+
+// Löschen des Files vom Server
+  if (!unlink($file)) // Löschen des Files und Überprüfung
+  {
+    echo ("Error deleting $file"); // Ausgabe bei Error
+  }
+  else
+  {
+    echo ("Deleted $file"); // Ausgabe wenn Erfolgreich
+  }
+}
+
+?>

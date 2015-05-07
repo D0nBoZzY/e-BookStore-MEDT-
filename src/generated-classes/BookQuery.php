1af4b8d0663fc845e8cc32b323727416560575e6
Lukas Zainzinger <lzainzinger@student.tgm.ac.at>
2015-05-07 09:10:45 +0200

    Delete Function in BookQuery.php

--- src/generated-classes/BookQuery.php
+++ src/generated-classes/BookQuery.php
@@ -15,4 +15,28 @@ use Base\BookQuery as BaseBookQuery;
 class BookQuery extends BaseBookQuery
 {
 
+  /*
+    Delete-Funktion um ein File aus dem Server und den dazugehörigen DB-Eintrag zu löschen
+    Autor: lzainzinger
+    Version: 2015-05-04
+  */
+  function delete($title){
+    $book = BookQuery::create()->filterByTitle($title);
+    $file = $book->getPath(); // Pfad zum File am Server
+
+
+  // Löschen des Datenbank - Eintrages
+    $book->delete(); // Löschen DB
+
+
+  // Löschen des Files vom Server
+    if (!unlink($file)) // Löschen des Files vom Server und Überprüfung
+    {
+      echo ("Error deleting $file"); // Ausgabe bei Error
+    }
+    else
+    {
+      echo ("Deleted $file"); // Ausgabe wenn Erfolgreich
+    }
+  }
 }

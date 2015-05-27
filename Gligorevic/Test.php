4e7c7131e1836c3d76d74f9447903ac0531e1616
ngligorevic <ngligorevic@student.tgm.ac.at>
2015-05-27 10:27:41 +0200

    v1.1

--- Gligorevic/Test.php
+++ Gligorevic/Test.php
@@ -1,3 +1,17 @@
 <?php
-echo "test";
+
+
+$zumtesten = "nenad123"; 
+$testergebnis = filter_var($zumtesten, FILTER_VALIDATE_INT);
+if ($testergebnis == false) 
+{ 
+    // invalide
+    echo "Typ entspricht NICHT dem erwarteten"; 
+} 
+else 
+{ 
+    // valid
+    echo "Typ korrekt"; 
+}
+
 ?>
